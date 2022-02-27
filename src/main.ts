dfb0d90d0df0e0a36530cfc58f6ee26bbc5754d4
vscing <vscing@foxmail.com>
2022-02-27 23:25:32 +0800

    ✨ feat:配置vant、store

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
+import { setupStore } from '@/store';
+import { router, setupRouter } from '@/router';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+async function bootstrap() {
+  // vue实例
+  const app = createApp(App);
+
+  // Pinia store
+  setupStore(app);
+
+  // vue-router
+  setupRouter(app);
+
+  // 挂载
+  app.mount('#app');
+}
+
+// 启动
+bootstrap();
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
-import vueJsx from '@vitejs/plugin-vue-jsx'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import styleImport, { VantResolve } from 'vite-plugin-style-import'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx({
-      
+    vueJsx({}),
+    styleImport({
+      resolves: [VantResolve()],
     })
   ]
 })
