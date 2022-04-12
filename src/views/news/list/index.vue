422d0924576cf28fd4e88317f311dee0d19f0d70
chenfei <2671515389@qq.com>
2022-04-13 03:29:45 +0800

    feat:订单页面完成和最新活动

--- src/views/news/list/index.vue
+++ src/views/news/list/index.vue
@@ -6,6 +6,7 @@
   const loading = ref<boolean>(false);
   const finished = ref<boolean>(true);
   const active = ref<number>(0);
+  const actives = ref<number>(0);
 
   const router = useRouter();
 
@@ -19,21 +20,41 @@
 
 <template>
   <VantTabs class="tabs-box" v-model:active="active">
-    <VantTab title="最新活动"></VantTab>
-    <VantTab title="合成策略"></VantTab>
+    <VantTab title="最新活动">
+        <VantList
+          v-model:loading="loading"
+          :finished="finished"
+          finished-text="没有更多了"
+          @load="onLoad"
+        >
+            <ul class="product-list">
+                <li class="product-data" v-for="(item, index) in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]" :key="index">
+                    <div class="product-img"><img class="swiper-img" src="https://theoneart-common.oss-cn-qingdao.aliyuncs.com/author/2535e32ac8848c5b591b8f5a3cd1d6ad1649767552807.jpg" width="174.22" height="104.53"/></div>
+                    <div class="product-desc">「萌芽熊冬雪季系列数字藏品」快照时间说明</div>
+                    <div class="product-time">2022/04/12 20:45:12</div>
+                </li>
+            </ul>
+        </VantList>
+    </VantTab>
+    <VantTab title="合成策略">
+        <div class="explain">
+              <VantList
+                v-model:loading="loading"
+                :finished="finished"
+                finished-text="没有更多了"
+                @load="onLoad"
+              >
+                  <VantTabs v-model:active="actives" type="card">
+                    <VantTab title="玩法一">内容 1</VantTab>
+                    <VantTab title="玩法二">内容 2</VantTab>
+                    <VantTab title="玩法三">内容 3</VantTab>
+                    <VantTab title="玩法四">内容 3</VantTab>
+                  </VantTabs>
+              </VantList>
+        </div>
+    </VantTab>
     <VantTab title="客服帮助"></VantTab>
   </VantTabs>
-
-  <VantList
-    v-model:loading="loading"
-    :finished="finished"
-    finished-text="没有更多了"
-    @load="onLoad"
-  >
-    <ul class="product-list">
-      
-    </ul>
-  </VantList>
 </template>
 
 <style lang="less" scoped>
@@ -42,13 +63,59 @@
       background-color: #01c2c3;
     }
   }
+  .explain{
+        background: #CBB895;
+        height: 1000px;
+        :deep(.van-tab--card.van-tab--active){
+          background-color: #ffffff;
+          height:80px;
+          border-radius: 10px;
+        }
+        :deep(.van-tab--card.van-tab--active){
+          color: #0E0B20;
+        }
+        :deep(.van-tabs__nav--card){
+          height:50px;
+          border: none;
+        }
+        :deep(.van-tabs--card>.van-tabs__wrap){
+          height:50px;
+        }
+  }
   .product-list {
     width: 100%;
     display: flex;
     flex-flow: row wrap;
-    justify-content: flex-start;
-    margin-top: var(--van-dropdown-menu-height);
+    margin-top: 20px;
     margin-bottom: 10px;
     padding: 0 10px;
+    .product-data{
+      width: 50%;
+      display: flex;
+      flex-flow: column wrap;
+      .product-img{
+          display: flex;
+          justify-content: center;
+          .swiper-img{
+             border-radius: 4px;
+          }
+      }
+      .product-desc{
+          width: 150px;
+          padding-top: 25px;
+          margin: 0 auto;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+      }
+      .product-time{
+          display: flex;
+          justify-content: center;
+          padding: 20px 0;
+          font-size: 12px;
+          font-weight: 400;
+          color: rgba(0,0,0,.4);
+      }
+    }
   }
 </style>
--- src/views/user/payment/fillmount/index.vue
+++ src/views/user/payment/fillmount/index.vue
@@ -10,16 +10,68 @@ import { reactive, ref, watch } from 'vue';
 import { getPayList } from '@/api/pay';
 import { List } from '@/components/List';
 import { Images } from '@/assets/images';
+import { formatToDateTime } from '@/utils/dateUtil';
 
 const router = useRouter();
 const tabTitle = reactive(['余额变动', '充值', '提现', '收益']);
-const active = ref(0);
+const active = ref<any>(0);
 
 const onClickLeft = () => router.go(-1);
 
 const params = reactive({
   type: 0
 });
+
+// const billType = (bill = 1) => {
+//     let bill_type = '';
+//       switch (bill) {
+//               case 1:
+//                 bill_type = '充值'
+//                 break;
+
+//               case 2:
+//                 bill_type = '体现'
+//                 break;
+
+//               case 3:
+//                 bill_type = '收益'
+//                 break;
+
+//               case 4:
+//                 bill_type = '佣金'
+//                 break;
+
+//               default:
+//                 break;
+//       }
+//       return bill_type;
+// }
+
+//支付方式
+const payment = (type) => {
+      let pay = '';
+      switch (type) {
+        case 1:
+          pay = '银行卡'
+          break;
+
+        case 2:
+          pay = '支付宝'
+          break;
+
+        case 3:
+          pay = '微信'
+          break;
+
+        case 4:
+          pay = '余额'
+          break;
+
+        default:
+          break;
+      }
+      return pay;
+}
 
 watch(active, (val: any, old: any) => {
   if(val !== old) {
@@ -42,8 +94,30 @@ const onClickTab = ({ title }) => console.log(title, active.value);
           :params="params"
         >
           <template #default="defaultProps">
-            <div v-for="item in defaultProps.list" class="item">
-              {{JSON.stringify(item)}}
+            <div v-for="item in defaultProps.list" class="item" :key="item.id">
+              <div class="bill">
+                  <div class="bill_payment">
+                      <div class="bill_name">{{payment(item.bill_type)}}</div>
+                      <div class="bill_value">已完成</div>
+                  </div>
+                  <div class="bill_type">
+                      <div class="bill_name">账单类型</div>
+                      <div class="bill_value">{{item.bill_name}}</div>
+                  </div>
+                  <div class="bill_amount">
+                      <div class="bill_name">支付金额</div>
+                      <div class="bill_value">￥{{item.bill_money}}</div>
+                  </div>
+                  <div class="bill_desc">
+                      <div class="bill_name">支付订单</div>
+                      <div class="bill_value">{{item.order_number}}</div>
+                  </div>
+                  <div class="bill_time">
+                      <div class="bill_name">支付时间</div>
+                      <div class="bill_value">{{formatToDateTime(item.created_at)}}</div>
+                  </div>
+              </div>
+              <!-- {{JSON.stringify(item)}} -->
             </div>
           </template>
           <template #empty>
@@ -77,7 +151,90 @@ const onClickTab = ({ title }) => console.log(title, active.value);
   }
 }
 .item {
-  height: 100px;
-  background-color: #ff0000;
+  // height: 100px;
+  margin-bottom: 15px;
+  // background-color: #ff0000;
+}
+.bill{
+  margin: 15px 10px;
+  background-color: #ffffff;
+  padding: 10px;
+  .bill_payment{
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      padding: 5px 0;
+      .bill_name{
+          color: #333;
+          font-weight: 700;
+          font-size: 16px;
+      }
+      .bill_value{
+          color: #01c2c3;
+          font-weight: 700;
+          font-size: 14px;
+      }
+  }
+  .bill_type{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        padding: 5px 0;
+        .bill_name{
+            color: #333;
+            font-size: 14px;
+        }
+        .bill_value{
+            font-weight: 700;
+            color: rgba(0, 0, 0, 0.4);
+            font-size: 14px;
+        }
+  }
+  .bill_amount{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        padding: 5px 0;
+        .bill_name{
+            color: #333;
+            font-size: 14px;
+        }
+        .bill_value{
+            font-weight: 700;
+            color: #f4a33a;
+            font-size: 14px;
+        }
+  }
+  .bill_desc{
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      padding: 5px 0;
+      .bill_name{
+         color: #333;
+      }
+      .bill_value{
+          font-weight: 700;
+          color: rgba(0, 0, 0, 0.4);
+          font-size: 14px;
+      }
+  }
+  .bill_time{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        padding: 5px 0;
+        .bill_name{
+          color: #333;
+        }
+        .bill_value{
+            font-weight: 700;
+            color: rgba(0, 0, 0, 0.4);
+            font-size: 14px;
+        }
+  }
+}
+:deep(.van-tabs__line){
+    background: #01c2c3;
 }
 </style>
